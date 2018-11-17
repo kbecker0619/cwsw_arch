24c1859ccc999ab18b8ca9bddb1b723759b5e28d
Kevin Becker <kbecke35@ford.com>
2018-11-17 18:58:08 -0500

    Added MSVC 2008 project

--- i386/src/cwsw_arch.c
+++ i386/src/cwsw_arch.c
@@ -19,9 +19,10 @@
 // ============================================================================
 
 #include "projcfg.h"
-#if !(  (XPRJ_Win_MinGW_Debug) ||	\
-        (XPRJ_Debug_Linux_GCC) ||	\
-        (XPRJ_NB_Debug) ||			\
+#if !(  (XPRJ_Win_MinGW_Debug)	||	\
+        (XPRJ_Debug_Linux_GCC)	||	\
+        (XPRJ_NB_Debug)			||	\
+		(XPRJ_MSVC_Debug)		||	\
         (XPRJ_CVI_Debug) )			/* { */
 #error Unknown CPU Architecture
 #endif															/* } */
